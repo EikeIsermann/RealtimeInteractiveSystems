694291d14b136446d1f7ed2f706155ed6e689539
Christian Treffs <ctreffs@gmail.com>
2013-11-05 17:07:08 +0100

    bla

--- src/main/scala/edges/Edge.scala
+++ src/main/scala/edges/Edge.scala
@@ -1,9 +1,18 @@
 package edges
 
+import java.util.UUID
+import nodes.Node
+import scala.collection.immutable.HashMap
+
 /**
  * Created by Christian Treffs
  * Date: 05.11.13 17:00
  */
-trait Edge {
+abstract class Edge(_head: Node, _tail: Node) {
+  val id = UUID.randomUUID()
+  val head: Node = _head
+  val tail: Node = _tail
+  val attributes: HashMap = HashMap.empty()
+
 
 }
--- src/main/scala/nodes/Node.scala
+++ src/main/scala/nodes/Node.scala
@@ -1,9 +1,15 @@
 package nodes
 
+import java.util.UUID
+import scala.collection.immutable.HashMap
+import edges.Edge
+
 /**
  * Created by Christian Treffs
  * Date: 05.11.13 17:00
  */
 trait Node {
+  val id = UUID.randomUUID()
+  val childNodes = HashMap.empty[Node, Edge]
 
 }
