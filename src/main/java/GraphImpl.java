1d205340ba8bfaaa86bc7670e8d2ef50200b2757
Christian Treffs <ctreffs@gmail.com>
2013-11-05 16:45:42 +0100

    ogl small changes

---
+++ src/main/java/GraphImpl.java
@@ -0,0 +1,7 @@
+public class GraphImpl {
+
+	public static void main(String[] args) {
+		
+	}
+	
+}
--- src/main/scala/HelloWorld.scala
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Christian Treffs
- * Date: 05.11.13 10:47
- */
-object HelloWorld {
-       def main(args: Array[String]) {
-         println("Hello World")
-       }
-}
