6f3b82eb112a27612eca96a33aaee6ca8890a1de
eike isermann <eike.isermann@gmail.com>
2014-03-20 12:02:20 +0100

    starting the scenegraph

---
+++ src/main/scala/architecture/GameObject.scala
@@ -0,0 +1,11 @@
+package main.scala.architecture
+
+/**
+ * User: uni
+ * Date: 18.03.14
+ * Time: 05:04
+ * This is a RIS Project class
+ */
+trait GameObject {
+
+}
---
+++ src/main/scala/gfx/scenegraph/GroupNode.scala
@@ -0,0 +1,11 @@
+package main.scala.gfx.scenegraph
+
+/**
+ * User: uni
+ * Date: 18.03.14
+ * Time: 04:54
+ * This is a RIS Project class
+ */
+class GroupNode {
+
+}
