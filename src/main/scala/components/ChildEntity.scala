143bf6eba636dfd6122871fde32fee9a3fc33c08
Christian Treffs <ctreffs@gmail.com>
2014-03-21 10:08:27 +0100

    parent child entity

--- src/main/scala/components/ChildEntity.scala
+++ src/main/scala/components/ChildEntity.scala
@@ -1,9 +1,16 @@
 package main.scala.components
 
+import main.scala.tools.Identifier
+import main.scala.architecture.{ComponentCreator, Component}
+import scala.xml.NodeSeq
+
 /**
  * Created by Christian Treffs
  * Date: 21.03.14 10:06
  */
-object ChildEntity {
-
+object ChildEntity extends ComponentCreator{
+  override def fromXML(xml: NodeSeq): Component = ???
+}
+case class ChildEntity(identifier: Identifier)  extends Component {
+  override def toXML: NodeSeq = ???
 }
--- src/main/scala/components/ParentEntity.scala
+++ src/main/scala/components/ParentEntity.scala
@@ -1,9 +1,18 @@
 package main.scala.components
 
+import main.scala.architecture.{ComponentCreator, Component}
+import scala.xml.NodeSeq
+import main.scala.tools.Identifier
+
+
 /**
  * Created by Christian Treffs
  * Date: 21.03.14 10:05
  */
-class ParentEntity {
 
+object ParentEntity extends ComponentCreator{
+  override def fromXML(xml: NodeSeq): Component = ???
+}
+case class ParentEntity(identifier: Identifier) extends Component{
+  override def toXML: NodeSeq = ???
 }
