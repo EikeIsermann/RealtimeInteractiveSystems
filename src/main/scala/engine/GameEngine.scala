bbebf0d2a842daa92f078e98aedee59a1b5a0d6e
Christian Treffs <ctreffs@gmail.com>
2014-05-14 17:13:53 +0200

    collision system error

--- src/main/scala/engine/GameEngine.scala
+++ src/main/scala/engine/GameEngine.scala
@@ -16,6 +16,7 @@ import main.scala.event.EntityRemoved
 import main.scala.systems.input.Triggers
 import main.scala.components.Camera
 import main.scala.event.EntityCreated
+import main.scala.systems.physics.CollisionSystem
 
 /**
  * Created by Christian Treffs
@@ -179,6 +180,7 @@ object GameEngine extends Engine with EventReceiver{
     //register systems with engine
     add(new CameraSystem)
     add(new RenderingSystem)
+    add(new CollisionSystem)
 
 
     Input.init()
