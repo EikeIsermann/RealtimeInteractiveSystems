32d3ed8d9850b1f8a340569a8ae782e27b5ddad0
Christian Treffs <ctreffs@gmail.com>
2014-05-20 15:07:32 +0200

    quat apply

--- src/main/scala/math/Quat.scala
+++ src/main/scala/math/Quat.scala
@@ -12,7 +12,7 @@ object Quat {
   def identity: Quat = new Quat()
   implicit def Quat(quat: ogl.vecmath.Quaternion) = new Quat(quat.x(), quat.y(), quat.z(), quat.w())
 
-  implicit def Quat(mat4f: Mat4f): Quat = new Quat().apply(mat4f)
+  implicit def apply(mat4f: Mat4f): Quat = new Quat().apply(mat4f)
 }
 
 final case class Quat(private val x1: Float = 0,private val y1: Float = 0,private val z1: Float = 0,private val w1: Float = 1) extends QuaternionImp(x1,y1,z1,w1) {
