e8776f141ec697007a4de2435b5175565352317f
Christian Treffs <ctreffs@gmail.com>
2014-05-22 22:53:42 +0200

    Navcell

--- src/main/scala/systems/ai/pathfinding/NavigationCell.scala
+++ src/main/scala/systems/ai/pathfinding/NavigationCell.scala
@@ -8,8 +8,18 @@ import main.scala.math.Vec3f
  * Time: 21:34
  * This is a RIS Project class
  */
-class NavigationCell(myPoints: Array[Float]){
+object NavigationCell {
+  def apply(p1: Vec3f, p2: Vec3f, p3: Vec3f): NavigationCell = new NavigationCell(p1,p2,p3)
+  def apply(cellPoints: Array[Vec3f]): NavigationCell = new NavigationCell(cellPoints)
 
-  var points = myPoints
+}
+
+class NavigationCell(cellPoints: Array[Vec3f]){
+
+  def this(p1: Vec3f, p2: Vec3f, p3: Vec3f) = this(Array(p1,p2,p3))
+
+  var points = cellPoints
+
+  def center(p1: Vec3f, p2: Vec3f): Vec3f = 0.5f*(p1+p2)
 
 }
